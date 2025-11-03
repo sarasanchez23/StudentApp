31dba9379cd5e5c8f88857a63532689dceed5a09
sarasanchez23 <sara23sanchez06@gmail.com>
2025-11-03 17:15:33 +0000

    exercise 6: add getGrade() method and print grade in main

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,3 @@
-class Student(
-    val id: String,
-    val name: String,
-    val course: String,
-    initialMark: Double
-) {
-    var mark: Double = 0.0
-        set(value) {
-            if (value in 0.0..100.0) {
-                field = value
-            } else {
-                println("Error: Invalid mark ($value). Must be 0..100.")
-                // no cambia el field
-            }
-        }
-
-    init {
-        // 👇 Esto SÍ usa el setter y por tanto valida initialMark
-        this.mark = initialMark
-    }
-
-    override fun toString() = "Name: $name, Course: $course, Mark: $mark"
-}
-
-
 fun main() {
     while (true) {
         print("Enter student name (or type 'quit' to stop): ")
@@ -49,7 +24,7 @@ fun main() {
         val student = Student(id, name, course, mark)
 
         // Mostramos los detalles
-        println("Student created: $student")
+        println("Student created: $student, Grade: ${student.getGrade()}\n")
         println("----------------------------------")
     }
 }
---
+++ src/Student.kt
@@ -0,0 +1,33 @@
+class Student(
+    val id: String,
+    val name: String,
+    val course: String,
+    initialMark: Double
+) {
+    var mark: Double = 0.0
+        set(value) {
+            if (value in 0.0..100.0) {
+                field = value
+            } else {
+                println("Error: Invalid mark ($value). Must be 0..100.")
+                // no cambia el field
+            }
+        }
+
+    init {
+        // 👇 Esto SÍ usa el setter y por tanto valida initialMark
+        this.mark = initialMark
+    }
+
+    fun getGrade(): String {
+        return when (mark) {
+            in 70.0..100.0 -> "First"
+            in 60.0..69.9 -> "2/1"
+            in 50.0..59.9 -> "2/2"
+            in 40.0..49.9 -> "Third"
+            else -> "Fail"
+        }
+    }
+
+    override fun toString() = "Name: $name, Course: $course, Mark: $mark"
+}
